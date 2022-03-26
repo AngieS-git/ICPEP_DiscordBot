e675434219097a8ebe7dc76eff2ee667a1f81f32
AngieS-git <m.angelinesamson@gmail.com>
2022-03-26 21:25:51 +0800

    added changes to main

--- main.js
+++ main.js
@@ -31,4 +31,23 @@ client.on("guildMemberAdd", async (member) =>{
     })
 })
 
+client.slashcommands = new Discord.Collection()
+
+client.loadSlashCommands = (bot, reload) => require("./handlers/slashcommands")(bot, reload)
+client.loadSlashCommands(bot, false)
+
+client.on("interactionCreate", (interaction) => {
+    if(!interaction.isCommand()) return
+    if(!interaction.isGuild()) return interaction.reply("This command can only be used in the server")
+
+    const slashcmd = client.slashcommands.get(interaction.commandName)
+
+    if(!slashcmd) return interaction.reply("Invalid slash command")
+
+    if(slashcmd.perms && !interaction.member.permissions.has(slashcommand.perm))
+        return interaction.reply("You do not have permissions for this command")
+
+    slashcmd.run(client, interaction)
+})
+
 client.login(process.env.TOKEN)
